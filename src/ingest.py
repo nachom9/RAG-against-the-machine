e11a6d3502c97e80627029836b9c25ef91462ba2
Nacho <nacho.melero.c@gmail.com>
2026-06-26 17:18:55 +0200

    chunking bugs fixed

--- src/ingest.py
+++ src/ingest.py
@@ -8,41 +8,42 @@ def get_chunks(path: str):
     for file in files:
         end_check = False
         last_char = 0
+        first_char = 0
         if file.endswith(".py"):
             with open(file, 'r') as f:
                 content = f.read()
-                first_char = 0
                 while not end_check and last_char < len(content):
-                    chunk_limit = first_char + 1999
-                    chunk = content[first_char:chunk_limit]
+                    chunk_limit = min(first_char + 1999, len(content))
                     last_char = (
-                        max(chunk[first_char:chunk_limit].rfind("class"),
-                            chunk[first_char:chunk_limit].rfind("def"))
+                        max(content[first_char:chunk_limit].rfind("class"),
+                            content[first_char:chunk_limit].rfind("def"))
                     )
-                    if last_char < 0:
+                    if last_char <= 0:
                         last_char = chunk_limit
+                    else:
+                        last_char += first_char
                     if last_char == len(content):
                         end_check = True
                     chunk = content[first_char:last_char]
-                    save_chunk(chunk, path, first_char, last_char)
-                    first_char = last_char + 1
+                    save_chunk(chunk, file, first_char, last_char)
+                    first_char = last_char
         elif file.endswith(".md"):
             with open(file, 'r') as f:
                 content = f.read()
-                first_char = 0
                 while not end_check and last_char < len(content):
-                    chunk_limit = first_char + 1999
-                    chunk = content[first_char:chunk_limit]
-                    last_char = chunk[first_char:chunk_limit].rfind("\n#")
-                    if last_char == -1:
-                        last_char = chunk[first_char:chunk_limit].rfind("\n##")
-                    if last_char < 0:
+                    chunk_limit = min(first_char + 1999, len(content))
+                    last_char = content[first_char:chunk_limit].rfind("\n# ")
+                    if last_char <= 0:
+                        last_char = content[first_char:chunk_limit].rfind("\n## ")
+                    if last_char <= 0:
                         last_char = chunk_limit
+                    else:
+                        last_char += first_char
                     if last_char == len(content):
                         end_check = True
                     chunk = content[first_char:last_char]
-                    save_chunk(chunk, path, first_char, last_char)
-                    first_char = last_char + 1
+                    save_chunk(chunk, file, first_char, last_char)
+                    first_char = last_char
 
 
 def save_chunk(content: str, path: str, first_char: int, last_char: int):
@@ -53,6 +54,8 @@ def save_chunk(content: str, path: str, first_char: int, last_char: int):
         last_character_index = last_char,
         text = content
     )
+    json_test = chunk.model_dump_json(indent=4)
+    print(json_test)
 
 
 def create_dir(path: str):
