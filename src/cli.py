91bf55dc59352846ed2647b6be0a164c2b0e3eeb
Ignacio Melero Carracedo <imelero-@c2r12s4.42madrid.com>
2026-06-30 20:29:44 +0200

    search_dataset done. Need to solve output format bug

--- src/cli.py
+++ src/cli.py
@@ -2,7 +2,7 @@ from src.ingest import Parser
 import fire
 import bm25s
 import json
-from .models import MinimalSearchResults, MinimalSource
+from .models import MinimalSearchResults, MinimalSource, UnansweredQuestion, StudentSearchResults
 
 class RAGAplication:
 
@@ -45,8 +45,47 @@ class RAGAplication:
         print(json_result)
 
 
-    def search_dataset(self):
-        pass
+    def search_dataset(self, dataset_path: str, output_path: str, k: int = 10):
+        
+        index_path = "data/processed/bm25_index"
+        chunks_path = "data/processed/chunks/all_chunks.json"
+        search_results_list = []
+        retriever = bm25s.BM25.load(index_path, load_corpus=True)
+        with open(chunks_path, 'r', encoding="utf-8") as f:
+            chunks = json.load(f)
+    
+        with open(dataset_path, 'r', encoding='utf-8') as f:
+            dataset = json.load(f)
+        for question in dataset['rag_questions']:
+            sources = []
+            query = question['question']
+            query_tokens = bm25s.tokenize(query, stopwords="english")
+            results = retriever.retrieve(query_tokens, k=min(k, len(chunks)), return_as="documents")
+            for r in results[0]:
+                chunk_data = chunks[r['id']]
+                validate_source = MinimalSource(
+                        file_path=chunk_data['file_path'],
+                        first_character_index=chunk_data['first_character_index'],
+                        last_character_index=chunk_data['last_character_index'],
+                        text=chunk_data['text']
+                )
+                sources.append(validate_source)
+            search_result = MinimalSearchResults(
+                question_id=question['question_id'],
+                question=query,
+                retrieved_sources=sources,
+                )
+            search_results_list.append(search_result)
+        
+        searchs = StudentSearchResults(
+            search_results=search_results_list,
+            k=k
+        )
+        dict_searchs = searchs.model_dump()
+        json_searchs = json.dumps(dict_searchs, indent=4, ensure_ascii=False)
+        with open(output_path, 'w', encoding='utf-8') as f:
+            json.dump(json_searchs, f, indent=4, ensure_ascii=False)
+        print(json_searchs)
 
     def answer(self):
         pass
